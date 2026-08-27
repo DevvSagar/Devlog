160cc24be61f0f95329d2967f3d0d9cbba7a6fe9
Devv Sagar <Devvsag@gmail.com>
2026-08-27 23:50:08 +0530

    Updated html pages

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Devlog-Blog")
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def root():
-    return {"Message" : "Your API is Running !!!"}
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
+]
 
 
+
+@app.get("/", include_in_schema=False)
+@app.get("/posts",  include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request,"home.html",{"posts": posts,"title":"home"})
+
+
+
+@app.get("/api/Posts")
+def get_posts():
+    return posts
