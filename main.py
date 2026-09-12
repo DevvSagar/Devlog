e9029b0bc4cabbe28ccabaec9e77571858bcca4a
Devv Sagar <devvsag@gmail.com>
2026-09-12 17:53:53 +0530

    Put and Patch Done

--- main.py
+++ main.py
@@ -171,6 +171,48 @@ def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
+@app.put("/api/posts/{post_id}",response_model=PostResponse)
+def update_post_full(post_id: int , post_data:PostCreate , db: Annotated[Session,Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    if post_data.user_id != post.user_id:
+        result = db.execute(select(models.User).where(models.User.id == post_data.user_id))
+        user = result.scalars().first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found",
+            )
+
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
+
+    db.commit()
+    db.refresh(post)
+    return post
+
+
+
+@app.patch("/api/posts/{post_id}",response_model=PostResponse)
+def update_post_partially(post_id: int , post_data:PostUpdate , db: Annotated[Session,Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    updated_post = post_data.model_dump(exclude_unset=True)
+    for field , value in updated_post.items():
+        setattr(post,field,value)
+
+    db.commit()
+    db.refresh(post)
+    return post
+
+
 
 # this handles the route error - if user goes to route which is not created !
 @app.exception_handler(StarletteHTTPException)
