8a306484c5a78c3f4da14979725bf32758e18510
Devv Sagar <devvsag@gmail.com>
2026-09-14 21:54:06 +0530

    Auth added

--- auth.py
+++ auth.py
@@ -8,14 +8,15 @@ from config import settings
 
 password_hash = PasswordHash.recommended()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/users/token")
 
 
-def hash_password(password : str) -> str:
+def hash_password(password: str) -> str:
     return password_hash.hash(password)
 
-def verify_password(plain_password:str , hashed_password:str) -> bool:
-    return password_hash.verify(plain_password,hash_password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
 
 ## create_access_token
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
