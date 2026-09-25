2146f7b0268f81cc2cfe77f75cd91a5150e9cbc2
Devv Sagar <devvsag@gmail.com>
2026-09-25 09:49:53 +0530

    applied pagination

--- schemas.py
+++ schemas.py
@@ -13,7 +13,6 @@ class UserCreate(UserBase):
 class UserUpdate(BaseModel):
     username: str | None= Field(default=None,min_length=1,max_length=50)
     email : EmailStr | None = Field(default=None,max_length=120)
-    image_file: str | None= Field(default=None,min_length=1,max_length=200)
 
 class Token(BaseModel):
     access_token : str
@@ -54,3 +53,11 @@ class PostResponse(PostBase):
     user_id : int
     date_posted : datetime
     author : UserPublic
+
+## Paginated Post Response Schema
+class PaginatedPostsResponse(BaseModel):
+    posts: list[PostResponse]
+    total: int
+    skip: int
+    limit: int
+    has_more: bool
